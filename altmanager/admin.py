51bf28f839793ba9bc7e956350035e09a634f82b
Aaron Kable <aaronkable@gmail.com>
2025-07-13 09:22:03 +0800

    Add admin for history

--- altmanager/admin.py
+++ altmanager/admin.py
@@ -11,7 +11,21 @@ class ConfigAdmin(SingletonModelAdmin):
 
 
 admin.site.register(AltCorpRecord)
-admin.site.register(AltCorpHistory)
+
+
+@admin.register(AltCorpHistory)
+class AltCorpHistoryAdmin(admin.ModelAdmin):
+    list_display = ["corporation_name", "owner_character_name", "target"]
+    search_fields = ["corporation_name", "owner_character_name", "target__name"]
+    raw_id_fields = [
+        "target",
+        "corporation",
+        "owner",
+        "sanctioner",
+        "approver",
+    ]
+
+
 admin.site.register(AltCorpTarget)
 
 admin.site.register(MainInMemberCorpFilter)
